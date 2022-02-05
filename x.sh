31c4cbf406eae84cbee42418c815cb27d9b118c6
张炎泼 <drdr.xp@gmail.com>
2022-02-05 23:31:59 +0800

    use md2zhihu @main

---
+++ x.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+md2zhihu \
+--repo https://github.com/drmingdrmer/drmingdrmer.github.io.git@tmp-assets \
+--code-width 600 \
+--platform zhihu \
+--md-output _md2zhihu/ \
+_posts/2015-07-28-close-shutdown.markdown
